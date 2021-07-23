d4033df4cc6528ab9bf429063e9ed333680a4dad
seows <seows@laivdata.com>
2021-07-23 09:28:24 +0900

    Form submisson and XHRS 파트

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -6,8 +6,43 @@ describe('Input form', () => {
     cy.focused().should('have.class', 'new-todo');
   });
 
-  it.only('Accepts input', () => {
+  it('Accepts input', () => {
     const typedText = 'Buy milk';
     cy.get('.new-todo').type(typedText).should('have.value', typedText);
+  });
+
+  context('Form submission', () => {
+    beforeEach(() => {
+      cy.server();
+    });
+    it('Adds a new todo on submit', () => {
+      const itemText = 'Buy eggs';
+      cy.route('POST', '/api/todos', {
+        name: itemText,
+        id: 1,
+        isComplete: false,
+      });
+
+      cy.get('.new-todo')
+        .type(itemText)
+        .type('{enter}')
+        .should('have.value', '');
+      cy.get('.todo-list li').should('have.length', 1).and('contain', itemText);
+    });
+
+    it('Shows an error message on a failed submission', () => {
+      cy.route({
+        url: '/api/todos',
+        method: 'POST',
+        status: 500,
+        response: {},
+      });
+
+      cy.get('.new-todo').type('test{enter}');
+
+      cy.get('.todo-list li').should('not.exist');
+
+      cy.get('.error').should('be.visible');
+    });
   });
 });
